73c5e7386947c6d0b4e83bdbaa97de5900f67181
Anusha Parida <153987492+Anusha26399@users.noreply.github.com>
2025-08-10 13:39:55 +0530

    Update index.js

--- nodeserver/index.js
+++ nodeserver/index.js
@@ -6,4 +6,3 @@ const app = express();
 app.use(cors());
 app.use('/upload', uploadRouter); // add this line
 
-app.listen(3000, () => console.log('Node server running on http://192.168.20.69:3000'));
